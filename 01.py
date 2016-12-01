5fbd6fef93c84426da0359f05d28059dab13fffd
Ales Kocjancic <alesdotio@gmail.com>
2016-12-01 12:51:15 +0100

    added day1 part2

--- 01.py
+++ 01.py
@@ -1,3 +1,4 @@
+
 NORTH = 0
 EAST = 1
 SOUTH = 2
@@ -7,6 +8,7 @@ WEST = 3
 def main():
     input_value = raw_input()
 
+    visited_positions = []
     movements = {
         NORTH: 0,
         EAST: 0,
@@ -24,12 +26,23 @@ def main():
         else:
             current_direction -= 1
 
-        current_direction = current_direction % 4  # normalize direction
+        current_direction %= 4  # normalize direction
+        all_moves = dict(movements)
         movements[current_direction] += amount
 
-    pos_x = abs(movements[NORTH] - movements[SOUTH])
-    pos_y = abs(movements[EAST] - movements[WEST])
-    print pos_x + pos_y
+        # part 2
+        for i in range(1, amount):
+            all_moves[current_direction] += 1
+            pos_x = all_moves[EAST] - all_moves[WEST]
+            pos_y = all_moves[NORTH] - all_moves[SOUTH]
+            if (pos_x, pos_y) in visited_positions:
+                print 'ALREADY VISITED!', abs(pos_x) + abs(pos_y)
+            else:
+                visited_positions.append((pos_x, pos_y))
+
+    move_x = abs(movements[EAST] - movements[WEST])
+    move_y = abs(movements[NORTH] - movements[SOUTH])
+    print 'TOTAL DISTANCE =', move_x + move_y
 
 
 if __name__ == '__main__':
